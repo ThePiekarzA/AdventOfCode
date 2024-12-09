0fad942c22fb90f6d1e0aad42200bb3137947928
ThePiekarzA <thepiekarza@gmail.com>
2024-12-09 08:59:38 +0100

    day 8 of 2024 part 2 WIP still

--- AdventOfCode/2024/06/GuardGallivant.cs
+++ AdventOfCode/2024/06/GuardGallivant.cs
@@ -1,5 +1,9 @@
-﻿using System.Formats.Tar;
+﻿using System.Drawing;
+using System.Formats.Tar;
 using System.Net.Http.Headers;
+using AdventOfCode._2023._05;
+using AdventOfCode._2023._10;
+using AdventOfCode.Common;
 using File = System.IO.File;
 
 namespace AdventOfCode._2024._06;
@@ -62,6 +66,327 @@ public class GuardGallivant
     public static int RunPartTwo()
     {
         return CountPossibleLoops(InputFilePath);
+        // 1441
+        // 1499
+    }
+
+    public static int CountPossibleLoops2(string inputFilePath)
+    {
+        var lines = File.ReadAllLines(inputFilePath);
+        var width = lines[0].Length;
+        var height = lines.Length;
+
+        var obstacles = GetAllObstacles(lines);
+        var possibleBarricades = CalculateAllPossibleBarricades(width, height, obstacles);
+
+        var dimensions = new Position()
+        {
+            X = lines[0].Length,
+            Y = lines.Length
+        };
+
+        var loopCount = 0;
+        var analyzed = 0;
+        var total = possibleBarricades.Count;
+        foreach (var barricade in possibleBarricades)
+        {
+            analyzed++;
+            Console.Write($"\nAnalyzing {analyzed} of {total} barricades ({((double)analyzed*100/total):F2}%) at {barricade}");
+
+            if( AnalyzeSingleLoopFinal(lines, dimensions, barricade))
+            {
+                loopCount++;
+                Console.Write($" - VALID!");
+            }
+        }
+
+        return loopCount;
+    }
+
+    private static bool AnalyzeSingleLoopFinal(string[] lines, Position dimensions, Point barricade)
+    {
+        var map = lines.Select(line => line.ToList()).ToList();
+        map[barricade.Y][barricade.X] = '#';
+
+        var guardPosition = LocateGuard(dimensions, map);
+        var guardDirection = Direction.Up;
+
+        var barricadeFound = false;
+        var hitObstacles = new List<Position>();
+        do
+        {
+            //if (RotateGuardWithLoopCheck(ref guardDirection, guardPosition, map, barricade, ref barricadeFound, hitObstacles))
+            //{
+            //    return true;
+            //}
+
+            MoveGuard(guardDirection, guardPosition);
+        } while (CheckBoundariesWithDirection(guardPosition, guardDirection, dimensions));
+
+        return false;
+    }
+
+    private static bool RotateGuardWithLoopCheck(ref Direction guardDirection, Position guardPosition, List<List<char>> map, Position barricade, ref bool barricadeFound, List<Position> hitObstacles)
+    {
+        switch (guardDirection)
+        {
+            case Direction.Up:
+                if (guardPosition.Y <= 0)
+                    break;
+
+                if (map[guardPosition.Y - 1][guardPosition.X] == '#')
+                {
+                    
+
+                    if (guardPosition.X == barricade.X && guardPosition.Y - 1 == barricade.Y)
+                    {
+                        if (!barricadeFound)
+                        {
+                            barricadeFound = true;
+                            hitObstacles.Clear();
+                        }                        
+                    }
+
+                    if (CheckCorner(hitObstacles, guardPosition, barricade, ref barricadeFound)) return true;
+
+                    guardDirection = (Direction)(((int)guardDirection + 1) % 4);
+                }
+                break;
+            case Direction.Right:
+                if (guardPosition.X >= map[0].Count - 1)
+                    break;
+
+                if (map[guardPosition.Y][guardPosition.X + 1] == '#')
+                {
+
+                    if (guardPosition.X + 1 == barricade.X && guardPosition.Y == barricade.Y)
+                    {
+                        if (!barricadeFound)
+                        {
+                            barricadeFound = true;
+                            hitObstacles.Clear();
+                        }
+                    }
+                    if (CheckCorner(hitObstacles, guardPosition, barricade, ref barricadeFound)) return true;
+
+                    //if (hitObstacles.Any(o => guardPosition.X + 1 == o.X && guardPosition.Y == o.Y))
+                    //    return true;
+
+                    //hitObstacles.Add(new Position() { X = guardPosition.X + 1, Y = guardPosition.Y });
+
+                    //if (guardPosition.X + 1 == barricade.X && guardPosition.Y == barricade.Y)
+                    //{
+                    //    if (!barricadeFound)
+                    //    {
+                    //        barricadeFound = true;
+                    //        hitObstacles.Clear();
+                    //    }
+                    //}
+
+                    //if (guardPosition.X + 1 == barricade.X && guardPosition.Y == barricade.Y)
+                    //{
+                    //    if (barricadeFound)
+                    //        return true;
+                    //    barricadeFound = true;
+                    //}
+
+                    guardDirection = (Direction)(((int)guardDirection + 1) % 4);
+                }
+                break;
+            case Direction.Down:
+                if (guardPosition.Y >= map.Count - 1)
+                    break;
+
+                if (map[guardPosition.Y + 1][guardPosition.X] == '#')
+                {
+
+                    if (guardPosition.X == barricade.X && guardPosition.Y + 1 == barricade.Y)
+                    {
+                        if (!barricadeFound)
+                        {
+                            barricadeFound = true;
+                            hitObstacles.Clear();
+                        }
+                    }
+                    if (CheckCorner(hitObstacles, guardPosition, barricade, ref barricadeFound)) return true;
+                    //if (hitObstacles.Any(o => guardPosition.X == o.X && guardPosition.Y + 1 == o.Y))
+                    //    return true;
+
+                    //hitObstacles.Add(new Position() { X = guardPosition.X, Y = guardPosition.Y + 1 });
+
+                    //if (guardPosition.X == barricade.X && guardPosition.Y + 1 == barricade.Y)
+                    //{
+                    //    if (!barricadeFound)
+                    //    {
+                    //        barricadeFound = true;
+                    //        hitObstacles.Clear();
+                    //    }
+                    //}
+
+                    //if (guardPosition.X == barricade.X && guardPosition.Y + 1 == barricade.Y)
+                    //{
+                    //    if (barricadeFound)
+                    //        return true;
+                    //    barricadeFound = true;
+                    //}
+
+                    guardDirection = (Direction)(((int)guardDirection + 1) % 4);
+                }
+                break;
+            case Direction.Left:
+                if (guardPosition.X <= 0)
+                    break;
+
+                if (map[guardPosition.Y][guardPosition.X - 1] == '#')
+                {
+                    
+
+                    if (guardPosition.X - 1 == barricade.X && guardPosition.Y == barricade.Y)
+                    {
+                        if (!barricadeFound)
+                        {
+                            barricadeFound = true;
+                            hitObstacles.Clear();
+                        }
+                    }
+                    if (CheckCorner(hitObstacles, guardPosition, barricade, ref barricadeFound)) return true;
+                    //if (hitObstacles.Any(o => guardPosition.X - 1 == o.X && guardPosition.Y == o.Y))
+                    //    return true;
+
+                    //hitObstacles.Add(new Position() { X = guardPosition.X - 1, Y = guardPosition.Y });
+
+                    //if (guardPosition.X - 1 == barricade.X && guardPosition.Y == barricade.Y)
+                    //{
+                    //    if (!barricadeFound)
+                    //    {
+                    //        barricadeFound = true;
+                    //        hitObstacles.Clear();
+                    //    }
+                    //}
+
+                    //if (guardPosition.X - 1 == barricade.X && guardPosition.Y == barricade.Y)
+                    //{
+                    //    if (barricadeFound)
+                    //        return true;
+                    //    barricadeFound = true;
+                    //}
+
+                    guardDirection = (Direction)(((int)guardDirection + 1) % 4);
+                }
+                break;
+        }
+
+        return false;
+    }
+
+    public static bool CheckCorner(List<Position> corners, Position guardPosition, Position barricade, ref bool barricadeFound)
+    {
+        if (corners.Any(o => guardPosition.X == o.X && guardPosition.Y == o.Y))
+            return true;
+
+        corners.Add(new Position() { X = guardPosition.X, Y = guardPosition.Y });
+
+        return false;
+    }
+
+    private static List<Point> CalculateAllPossibleBarricades(int width, int height, List<Point> obstacles)
+    {
+        var possibleBarricades = new List<Point>();
+        foreach (var combination in GetObstacleCombinations(obstacles))
+        {
+            if (combination[0].X == combination[1].X ||
+                combination[0].Y == combination[1].Y)
+                continue;
+
+            if (combination[0].Y < combination[1].Y)
+            {
+                CalculateBarricades(combination, 0, width, height, possibleBarricades, obstacles);
+            }
+            else
+            {
+                CalculateBarricades(combination, 1, width, height, possibleBarricades, obstacles);
+            }
+        }
+
+        return possibleBarricades.Distinct().ToList();
+    }
+
+    private static IEnumerable<Point[]> GetObstacleCombinations(List<Point> obstacles)
+    {
+        foreach (var combination in Combinations.GetCombinations(obstacles.Count))
+        {
+            yield return combination.Select(e => obstacles[e]).ToArray();
+        }
+    }
+
+    private static void CalculateBarricades(Point[] positions, int upperIndex, int width, int height, List<Point> possibleBarricades, List<Point> obstacles)
+    {
+        var xDiff = Math.Abs(positions[0].X - positions[1].X);
+        var yDiff = Math.Abs(positions[0].Y - positions[1].Y);
+
+        Point firstBarricade, secondBarricade;
+        if (positions[upperIndex].X < positions[(upperIndex + 1) % 2].X) // upper-left
+        {
+            firstBarricade = new()
+            {
+                X = positions[upperIndex].X + xDiff + 1,
+                Y = positions[upperIndex].Y + 1
+            };
+
+            secondBarricade = new()
+            {
+                X = positions[upperIndex].X - 1,
+                Y = positions[upperIndex].Y + (yDiff - 1)
+            };
+        }
+        else // upper-right
+        {
+            firstBarricade = new()
+            {
+                X = positions[upperIndex].X - (xDiff - 1),
+                Y = positions[upperIndex].Y - 1
+            };
+
+            secondBarricade = new()
+            {
+                X = positions[upperIndex].X - 1,
+                Y = positions[upperIndex].Y + (yDiff + 1)
+            };
+        }
+
+        ValidateBarricade(width, height, possibleBarricades, obstacles, firstBarricade);
+        ValidateBarricade(width, height, possibleBarricades, obstacles, secondBarricade);
+    }
+
+    private static void ValidateBarricade(int width, int height, List<Point> possibleBarricades, List<Point> obstacles, Point barricade)
+    {
+        if (barricade.X >= 0 && barricade.X < width &&
+                    barricade.Y >= 0 && barricade.Y < height &&
+                    !obstacles.Any(o => o.X == barricade.X && o.Y == barricade.Y))
+        {
+            possibleBarricades.Add(barricade);
+        }
+    }
+
+    private static List<Point> GetAllObstacles(string[] lines)
+    {
+        var obstacles = new List<Point>();
+        for (int y = 0; y < lines.Length; y++)
+        {
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                if (lines[y][x] == '#')
+                {
+                    var obstacle = new Point()
+                    {
+                        X = x,
+                        Y = y
+                    };
+                    obstacles.Add(obstacle);
+                }
+            }
+        }
+        return obstacles;
     }
 
     public static int CountPossibleLoops(string inputFilePath)
@@ -82,11 +407,15 @@ public class GuardGallivant
             }
         } while (barricadeLocated);
 
+
         return loopCount;
     }
 
     private static bool AnalyzeSingleLoop(string[] rawFile, List<Position> possibleBarricades, ref bool barricadeLocated)
-    {
+    {       
+        var barricadeFound = false;
+        var hitObstacles = new List<Position>();
+
         var map = rawFile.Select(line => line.ToList()).ToList();
         var dimensions = new Position()
         {
@@ -107,7 +436,14 @@ public class GuardGallivant
         {
             if (!barricadeLocated && LocateBarricade2(guardDirection, map, guardPosition, currentBarricade, possibleBarricades))
                 barricadeLocated = true;
-            
+
+            if (barricadeLocated)
+            {
+                if (RotateGuardWithLoopCheck(ref guardDirection, guardPosition, map, currentBarricade, ref barricadeFound, hitObstacles))
+                {
+                    return true;
+                }
+            }
             RotateGuard(ref guardDirection, guardPosition, map);
 
             if (!barricadeLocated && LocateBarricade2(guardDirection, map, guardPosition, currentBarricade, possibleBarricades))
@@ -115,11 +451,11 @@ public class GuardGallivant
 
             MoveGuard(guardDirection, guardPosition);
 
-            if (CheckIfLooped(guardDirection, guardPosition, currentBarricade))
-            {
-                Console.WriteLine($"{currentBarricade}");
-                return true;
-            }
+            //if (CheckIfLooped(guardDirection, guardPosition, currentBarricade))
+            //{
+            //    Console.WriteLine($"{currentBarricade}");
+            //    return true;
+            //}
 
         } while (CheckBoundariesWithDirection(guardPosition, guardDirection, dimensions));
 
