3fd85faf98c178e7e3edc3b3a08d154a2dfbbd4f
ThePiekarzA <thepiekarza@gmail.com>
2025-12-06 14:20:32 +0100

    removed puzzle text

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,111 +1,111 @@
 ﻿#region 2023
 //using AdventOfCode._01.Trebuchet;
-//var answer = Trebuchet.RunPartOne(); // 55971
-//var answer = Trebuchet.RunPartTwo(); // 54719
+//var answer = Trebuchet.RunPartOne();
+//var answer = Trebuchet.RunPartTwo();
 
 //using AdventOfCode._01.NotQuiteLisp;
-//var answer = NotQuiteLisp.RunPartOne(); // 138
-//var answer = NotQuiteLisp.RunPartTwo(); // 1771
+//var answer = NotQuiteLisp.RunPartOne();
+//var answer = NotQuiteLisp.RunPartTwo();
 
 //using AdventOfCode._02;
-//var answer = CubeConundrum.RunPartOne(); // 2528
-//var answer = CubeConundrum.RunPartTwo(); // 67363
+//var answer = CubeConundrum.RunPartOne();
+//var answer = CubeConundrum.RunPartTwo();
 
 //using AdventOfCode._03;
-//var answer = GearRatios.RunPartOne(); // 532331
-//var answer = GearRatios.RunPartTwo(); // 82301120
+//var answer = GearRatios.RunPartOne();
+//var answer = GearRatios.RunPartTwo();
 
 //using AdventOfCode._04;
-//var answer = Scratchcards.RunPartOne(); // 26426
-//var answer = Scratchcards.RunPartTwo(); // 6227972
+//var answer = Scratchcards.RunPartOne();
+//var answer = Scratchcards.RunPartTwo();
 
 //using AdventOfCode._05;
-//var answer = IfYouGiveASeedAFertilizer.RunPartOne(); // 340994526
-//var answer = IfYouGiveASeedAFertilizer.RunPartTwo(); // 52210644
+//var answer = IfYouGiveASeedAFertilizer.RunPartOne();
+//var answer = IfYouGiveASeedAFertilizer.RunPartTwo();
 
 //using AdventOfCode._06;
-//var answer = WaitForIt.RunPartOne(); // 2756160
-//var answer = WaitForIt.RunPartTwo(); // 34788142
+//var answer = WaitForIt.RunPartOne();
+//var answer = WaitForIt.RunPartTwo();
 
 //using AdventOfCode._07;
-//var answer = CamelCards.RunPartOne(); // 253910319
-//var answer = CamelCards.RunPartTwo(); // 254083736
+//var answer = CamelCards.RunPartOne();
+//var answer = CamelCards.RunPartTwo();
 
 //using AdventOfCode._08;
-//var answer = HauntedWasteland.RunPartOne(); // 22411
-//var answer = HauntedWasteland.RunPartTwo(); // 11188774513823
+//var answer = HauntedWasteland.RunPartOne();
+//var answer = HauntedWasteland.RunPartTwo();
 
 //using AdventOfCode._09;
-//var answer = MirageMaintenance.RunPartOne(); // 1731106378
-//var answer = MirageMaintenance.RunPartTwo(); // 1087
+//var answer = MirageMaintenance.RunPartOne();
+//var answer = MirageMaintenance.RunPartTwo();
 
 //using AdventOfCode._10;
-//var answer = PipeMaze.RunPartOne(); // 6864
-//var answer = PipeMaze.RunPartTwo(); // 349
+//var answer = PipeMaze.RunPartOne();
+//var answer = PipeMaze.RunPartTwo();
 
 //using AdventOfCode._11;
-//var answer = CosmicExpansion.RunPartOne(); // 9509330
-//var answer = CosmicExpansion.RunPartTwo(); // 635832237682
+//var answer = CosmicExpansion.RunPartOne();
+//var answer = CosmicExpansion.RunPartTwo();
 
 //using AdventOfCode._12;
-//var answer = HotSprings.RunPartOne(); // 7460
+//var answer = HotSprings.RunPartOne();
 #endregion
 
 #region 2024
 
 //using AdventOfCode._2024._01;
-//var answer = HistorianHysteria.RunPartOne(); // 2375403
-//var answer = HistorianHysteria.RunPartTwo(); // 23082277
+//var answer = HistorianHysteria.RunPartOne();
+//var answer = HistorianHysteria.RunPartTwo();
 
 //using AdventOfCode._2024._02;
-//var answer = RedNosedReports.RunPartOne(); // 631
-//var answer = RedNosedReports.RunPartTwo(); // 665
+//var answer = RedNosedReports.RunPartOne();
+//var answer = RedNosedReports.RunPartTwo();
 
 //using AdventOfCode._2024._03;
-//var answer = MullItOver.RunPartOne(); // 187825547
-//var answer = MullItOver.RunPartTwo(); // 85508223
+//var answer = MullItOver.RunPartOne();
+//var answer = MullItOver.RunPartTwo();
 
 //using AdventOfCode._2024._04;
-//var answer = CeresSearch.RunPartOne(); // 2654
-//var answer = CeresSearch.RunPartTwo(); // 1990
+//var answer = CeresSearch.RunPartOne();
+//var answer = CeresSearch.RunPartTwo();
 
 //using AdventOfCode._2024._05;
-//var answer = PrintQueue.RunPartOne(); // 4135
-//var answer = PrintQueue.RunPartTwo(); // 5285
+//var answer = PrintQueue.RunPartOne();
+//var answer = PrintQueue.RunPartTwo();
 
 //using AdventOfCode._2024._06;
-//var answer = GuardGallivant.RunPartOne(); // 4663
+//var answer = GuardGallivant.RunPartOne();
 //var answer = GuardGallivant.RunPartTwo(); // TODO: Check other noted idea for solving this nightmare 
 
 //using AdventOfCode._2024._07;
-//var answer = BridgeRepair.RunPartOne(); // 2654749936343
-//var answer = BridgeRepair.RunPartTwo(); // 124060392153684
+//var answer = BridgeRepair.RunPartOne();
+//var answer = BridgeRepair.RunPartTwo();
 
 //using AdventOfCode._2024._08;
-//var answer = ResonantCollinearity.RunPartOne(); // 303
-//var answer = ResonantCollinearity.RunPartTwo(); // 1045
+//var answer = ResonantCollinearity.RunPartOne();
+//var answer = ResonantCollinearity.RunPartTwo();
 
 //using AdventOfCode._2024._09;
-//var answer = DiskFragmenter.RunPartOne(); // 6299243228569
-//var answer = DiskFragmenter.RunPartTwo(); // 6326952672104
+//var answer = DiskFragmenter.RunPartOne();
+//var answer = DiskFragmenter.RunPartTwo();
 
 #endregion
 
 //using AdventOfCode._2025._01;
-//var answer = SecretEntrance.RunPartOne(); // 1021
-//var answer = SecretEntrance.RunPartTwo(); // 5933
+//var answer = SecretEntrance.RunPartOne();
+//var answer = SecretEntrance.RunPartTwo();
 
 //using AdventOfCode._2025._02;
-//var answer = GiftShop.RunPartOne(); // 26255179562
-//var answer = GiftShop.RunPartTwo(); // 31680313976
+//var answer = GiftShop.RunPartOne();
+//var answer = GiftShop.RunPartTwo();
 
 //using AdventOfCode._2025._03;
-//var answer = Lobby.RunPartOne(); // 16858
-//var answer = Lobby.RunPartTwo(); // 167549941654721
+//var answer = Lobby.RunPartOne();
+//var answer = Lobby.RunPartTwo();
 
 //using AdventOfCode._2025._04;
-//var answer = PrintingDepartment.RunPartOne(); // 1508
-//var answer = PrintingDepartment.RunPartTwo(); 
+//var answer = PrintingDepartment.RunPartOne();
+//var answer = PrintingDepartment.RunPartTwo();
 
 using AdventOfCode._2025._05;
 //var answer = Cafeteria.RunPartOne();
